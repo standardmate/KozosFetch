86fa24515a3e21e7be6d4fa783bdf8037a26723d
dhemeira <forex.craft11@gmail.com>
2021-12-07 17:11:22 +0100

    main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 "use strict"
+
+let url = "https://api.notion.com/v1/pages/Reading-List-380890c575c147c290663bbc94554870"
+
+fetch(url, {
+    method: "GET",
+    headers: {
+        Authorization: "Bearer " + token
+    }
+})
+.then((resp) => resp.json())
+.then((data) => {
+    console.log(data)
+})
