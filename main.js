bf378d9e79ba86d0efd27c6c22d868445e4fa17a
dhemeira <forex.craft11@gmail.com>
2021-12-07 20:03:43 +0100

    ora vege

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 "use strict"
 
-let url = "https://api.notion.com/v1/pages/Reading-List-380890c575c147c290663bbc94554870"
+let url = "https://api.notion.com/v1/pages/380890c575c147c290663bbc94554870"
 
 fetch(url, {
     method: "GET",
     headers: {
-        Authorization: "Bearer " + token
-    }
+        'Authorization': `Bearer ${token}`,
+        'Notion-Version': "2021-08-16"
+    },
+    // mode: "no-cors",
 })
 .then((resp) => resp.json())
 .then((data) => {
